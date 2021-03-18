97a60ca08e379710b6e34c411695bcb080f4cc11
karina liauw <kliauw@student.unimelb.edu.au>
2021-03-18 22:06:50 +1100

    added token objects to board from input json file

--- search/board.py
+++ search/board.py
@@ -4,13 +4,13 @@ class Board:
 	tokens = []
 	blocked_hex = None
 	radius = None
-	2d_array = None
+	two_d = None
 
 	#class contructor
 	def __init__(self, radius):
 		self.radius = radius
 
-	def create_array():
+	#def create_array():
 		
 	#set a hex to be blocked
 	def set_blocked_hex(position):
@@ -21,4 +21,4 @@ class Board:
 		self.tokens.append(token)
 
 	#check if the position is inside the board range
-	def inside_board(position):
+	#def inside_board(position):
--- search/main.py
+++ search/main.py
@@ -1,3 +1,4 @@
+
 """
 COMP30024 Artificial Intelligence, Semester 1, 2021
 Project Part A: Searching
@@ -9,6 +10,9 @@ This script contains the entry point to the program (the code in
 import sys
 import json
 from .token import Token
+from .board import Board
+
+# from .board import Board
 
 # If you want to separate your code into separate files, put them
 # inside the `search` directory (like this one and `util.py`) and
@@ -23,11 +27,35 @@ def main():
         print("usage: python3 -m search path/to/input.json", file=sys.stderr)
         sys.exit(1)
 
-    board = Board(4);
-    
-    token1 = Token((1, 1), 's', 'upper')
-    print(token1.category)
-    print(token1.team)
+    # board = Board(4);
+
+    # bikin tokennya
+    # mskin ke board
+
+    dict = {}
+    count = 1
+
+    tokens_list = []
+    # makes object (token) from json file
+    for i in data.keys():
+        #print(data[i]);
+        for j in data[i]:
+            #print(j);
+            token = Token((j[1],j[2]), j[0], i);
+            tokens_list.append(token)
+            #how to make multiple tokens?
+            print(count, token.category, token.team, token.position)
+            count += 1
+    print(tokens_list)
+
+    # adds tokens to board
+    board_state = Board(tokens_list)
+    print(board_state)
+
+    #token1 = Token((1, 1), 's', 'upper')
+    #print(token1.category)
+    #print(token1.team)
+
     # TODO:
     # Find and print a solution to the board configuration described
     # by `data`.
