b43bc648ddf450d6ff1ef56da8cb4838eea1f368
Erdinan Angkajaya <angkajayae@student.unimelb.edu.au>
2021-05-08 20:57:29 +1000

    change file structure

--- Project_B/skeleton-code-B/liauw/team_strategy/token.py
+++
@@ -1,7 +0,0 @@
-class Token:
-
-	#class constructor
-	def __init__(self, position, category, team):
-		self.position = position # (x, y)
-		self.category = category # r, p, s
-		self.team = team # 'upper' or 'lower'
